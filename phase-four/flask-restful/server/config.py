383816a541fd764321b4ccf9c2e20fe4ab1e1a22
Emiley Palmquist <emiley.palmquist@gmail.com>
2024-06-06 11:24:07 -0700

    DONE: flask restful lecture

--- phase-four/flask-restful/server/config.py
+++ phase-four/flask-restful/server/config.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
 from sqlalchemy import MetaData
 from flask_migrate import Migrate
+from flask_restful import Api
 
 convention = {
   "ix": "ix_%(column_0_label)s",
@@ -22,3 +23,5 @@ db = SQLAlchemy(metadata=metadata)
 migrate = Migrate(app, db)
 
 db.init_app(app)
+
+api = Api(app)
